fac66ee60f2d4eab4a50f28dfc67ef39e5cd7a00
Nail Burkeev <n.burkeev@mail.ru>
2022-05-20 01:48:57 +0500

    Adds using library without selected photos

--- Stickers-Creator/MainModule/View/PickerExtensions.swift
+++ Stickers-Creator/MainModule/View/PickerExtensions.swift
@@ -8,6 +8,7 @@
 import Foundation
 import PhotosUI
 
+// MARK: - Create Alerts
 extension MainViewController {
     func showWarningAlert(message: String) {
         let alert = UIAlertController(title: "Warning!", message: message, preferredStyle: .alert)
@@ -28,6 +29,7 @@ extension MainViewController {
     }
 }
 
+// MARK: - Working with Photo LIbrary
 extension MainViewController: PHPickerViewControllerDelegate {
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
         picker.dismiss(animated: true)
@@ -36,6 +38,7 @@ extension MainViewController: PHPickerViewControllerDelegate {
                 if let image = object as? UIImage {
                     DispatchQueue.main.async {
                         self.imageView.image = image
+                        self.view.addSubview(self.imageView)
                     }
                 }
             }
@@ -45,34 +48,46 @@ extension MainViewController: PHPickerViewControllerDelegate {
     func openGallery() {
         presenter?.getPhotoLibraryAccessPermission(complition: { status in
             if #available(iOS 14, *) {
-//                switch status {
-//                case .notDetermined:
-//
-//                case .restricted:
-//                    <#code#>
-//                case .denied:
-//                    self.showWarningAlertToApplicationSettings(message: "Please allow access to your Photo Gallery")
-//                case .authorized:
-//                    <#code#>
-//                case .limited:
-//                    PHPhotoLibrary.shared().presentLimitedLibraryPicker(from: self)
-//                @unknown default:
-//                    <#fatalError()#>
+                
+                switch status {
+                case .notDetermined, .denied:
+                    self.showWarningAlertToApplicationSettings(message: "Please allow access to your Photo Gallery")
+                case .restricted:
+                    self.showWarningAlert(message: "The application can't help you create stickers without access to your Photo Gallery :(")
+                case .authorized:
+                    var config = PHPickerConfiguration()
+                    config.selectionLimit = 1
+                    config.filter = .images
+                    
+                    let vc = PHPickerViewController(configuration: config)
+                    vc.delegate = self
+                    self.present(vc, animated: true)
+                case .limited:
+                    PHPhotoLibrary.shared().presentLimitedLibraryPicker(from: self)
+                default:
+                    fatalError("Unknown PHAuthorizationStatus")
+                }
+                
+            } else {
+// FIXME: - Add and check if it works +  add check permission
+                
+//                if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+//                    let imagePickerController = UIImagePickerController()
+//                    imagePickerController.delegate = self
+//                    imagePickerController.sourceType = .photoLibrary
+//                    self.present(imagePickerController, animated: true)
 //                }
+                fatalError("iOS version lower than 14.0")
             }
-
         })
         
-//        var config = PHPickerConfiguration()
-//        config.selectionLimit = 1
-//        config.filter = .images
-//
-//        let vc = PHPickerViewController(configuration: config)
-//        vc.delegate = self
-//        self.present(vc, animated: true)
+//    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//        self.dismiss(animated: true)
+//    }
     }
 }
 
+// MARK: - Working with camera
 extension MainViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
