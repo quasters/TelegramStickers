a33e85f726aeb4c5c028e07746a4ae35334ad24d
quasters <n.burkeev@mail.ru>
2022-05-30 18:15:53 +0500

    fix UIScrollView, bottomButtons

--- Stickers-Creator/MainModule/View/MainViewController.swift
+++ Stickers-Creator/MainModule/View/MainViewController.swift
@@ -16,10 +16,10 @@ class MainViewController: UIViewController {
     var workspaceScrollView = UIScrollView()
     var textVC = UIView()
     var stackView = UIStackView()
+    var brushSizeSlider = UISlider()
     var workspaceImageView = UIImageView()
     
     fileprivate let bottomButtonsImages = [ "pencil.circle", "scissors.circle", "circle.bottomhalf.filled",  "eye.circle", "folder.circle"]
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +35,6 @@ class MainViewController: UIViewController {
 
 // MARK: - Bottom buttons
 extension MainViewController {
-
     func setUpBottomButtons() {
         let buttons = setUpButtons()
         stackView = UIStackView(arrangedSubviews: buttons)
@@ -51,6 +50,8 @@ extension MainViewController {
             stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             stackView.heightAnchor.constraint(equalToConstant: 50),
         ])
+        
+        setupSlider()
     }
 
     func setUpButtons() -> [UIButton] {
@@ -70,80 +71,28 @@ extension MainViewController {
         button.setImage(image, for: .normal)
         return button
     }
-}
-
-// MARK: - UIScrollView
-extension MainViewController: UIScrollViewDelegate {
-    func configurateWorkspace(image: UIImage) {
-        workspaceScrollView.minimumZoomScale = 1
-        workspaceScrollView.maximumZoomScale = 5
-        workspaceScrollView.panGestureRecognizer.minimumNumberOfTouches = 2
-        workspaceScrollView.panGestureRecognizer.maximumNumberOfTouches = 2
-        workspaceScrollView.zoomScale = 1
-        workspaceScrollView.flashScrollIndicators()
-        workspaceScrollView.bounces = true
-        workspaceScrollView.delegate = self
-
-
-        self.view.addSubview(workspaceScrollView)
-        workspaceScrollView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            workspaceScrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
-            workspaceScrollView.bottomAnchor.constraint(equalTo: stackView.topAnchor, constant: -10),
-            workspaceScrollView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            workspaceScrollView.widthAnchor.constraint(equalTo: self.view.widthAnchor)
-        ])
-        configurateWorkspaceImage(image: image)
-    }
     
-    func configurateWorkspaceImage(image: UIImage) {
-        workspaceImageView = UIImageView()
-        workspaceImageView.image = image
-        workspaceImageView.backgroundColor = .blue
-        workspaceImageView.clipsToBounds = false
-        workspaceImageView.contentMode = .scaleAspectFit
+    func setupSlider() {
+        brushSizeSlider.minimumValue = 1.0
+        brushSizeSlider.maximumValue = 30.0
+        brushSizeSlider.value = 15.0
+        //brushSizeSlider.backgroundColor = .red
+        brushSizeSlider.addTarget(self, action: #selector(changeBrushSize), for: .valueChanged)
         
-        workspaceScrollView.addSubview(workspaceImageView)
-        
-        workspaceImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(brushSizeSlider)
+        brushSizeSlider.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            workspaceImageView.widthAnchor.constraint(equalTo: workspaceScrollView.widthAnchor),
-            workspaceImageView.heightAnchor.constraint(equalTo: workspaceScrollView.heightAnchor)
+            brushSizeSlider.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 20),
+            brushSizeSlider.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -20),
+            brushSizeSlider.heightAnchor.constraint(equalToConstant: 30),
+            brushSizeSlider.bottomAnchor.constraint(equalTo: stackView.topAnchor, constant: -15)
         ])
     }
     
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return workspaceImageView
-    }
-    
-    fileprivate func showEmptyScrollViewNotice() {
-        let width = self.view.bounds.width
-        let height = self.view.bounds.height
+    @objc func changeBrushSize() {
         
-        textVC = UIView(frame: CGRect(x: 0, y: 0, width: width, height: height))
-        let title = setEmptyScrollViewMessage("Create your new Sticker", size: 30, offset: -37)
-        let message = setEmptyScrollViewMessage("By clicking the Camera Button above, you can select an image from the Photo Gallery or Take a Photo", size: 17, offset: +25)
-        textVC.addSubview(title)
-        textVC.addSubview(message)
-        self.view.addSubview(textVC)
-    }
-    
-    fileprivate func setEmptyScrollViewMessage(_ message: String, size: CGFloat, offset: CGFloat) -> UILabel {
-        let width = self.view.bounds.width - 40
-        let height = self.view.bounds.height
-        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: height))
-        messageLabel.text = message
-        messageLabel.textColor = .gray
-        messageLabel.numberOfLines = 0
-        messageLabel.textAlignment = .center
-        messageLabel.font = UIFont(name: "Arial", size: size)
-        messageLabel.sizeToFit()
-        messageLabel.center.x = self.view.center.x
-        messageLabel.center.y = self.view.center.y + offset
-        return messageLabel
     }
 }
-
 
 // MARK: - Configure NavigationView
 extension MainViewController {
---
+++ Stickers-Creator/MainModule/View/UIScrollView.swift
@@ -0,0 +1,79 @@
+//
+//  UIScrollView.swift
+//  Stickers-Creator
+//
+//  Created by Наиль Буркеев on 30.05.2022.
+//
+
+import UIKit
+
+extension MainViewController: UIScrollViewDelegate {
+    func configurateWorkspace(image: UIImage) {
+        workspaceScrollView.minimumZoomScale = 1
+        workspaceScrollView.maximumZoomScale = 20
+        workspaceScrollView.panGestureRecognizer.minimumNumberOfTouches = 2
+        workspaceScrollView.panGestureRecognizer.maximumNumberOfTouches = 2
+        workspaceScrollView.zoomScale = 1
+        workspaceScrollView.flashScrollIndicators()
+        workspaceScrollView.bounces = true
+        workspaceScrollView.delegate = self
+
+
+        self.view.addSubview(workspaceScrollView)
+        workspaceScrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            workspaceScrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            workspaceScrollView.bottomAnchor.constraint(equalTo: brushSizeSlider.topAnchor, constant: -10),
+            workspaceScrollView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            workspaceScrollView.widthAnchor.constraint(equalTo: self.view.widthAnchor)
+        ])
+        configurateWorkspaceImage(image: image)
+    }
+    
+    func configurateWorkspaceImage(image: UIImage) {
+        workspaceImageView = UIImageView()
+        workspaceImageView.image = image
+        //workspaceImageView.backgroundColor = .blue
+        workspaceImageView.clipsToBounds = false
+        workspaceImageView.contentMode = .scaleAspectFit
+        
+        workspaceScrollView.addSubview(workspaceImageView)
+        
+        workspaceImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            workspaceImageView.widthAnchor.constraint(equalTo: workspaceScrollView.widthAnchor),
+            workspaceImageView.heightAnchor.constraint(equalTo: workspaceScrollView.heightAnchor)
+        ])
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return workspaceImageView
+    }
+    
+    func showEmptyScrollViewNotice() {
+        let width = self.view.bounds.width
+        let height = self.view.bounds.height
+        
+        textVC = UIView(frame: CGRect(x: 0, y: 0, width: width, height: height))
+        let title = setEmptyScrollViewMessage("Create your new Sticker", size: 30, offset: -37)
+        let message = setEmptyScrollViewMessage("By clicking the Camera Button above, you can select an image from the Photo Gallery or Take a Photo", size: 17, offset: +25)
+        textVC.addSubview(title)
+        textVC.addSubview(message)
+        self.view.addSubview(textVC)
+    }
+    
+    fileprivate func setEmptyScrollViewMessage(_ message: String, size: CGFloat, offset: CGFloat) -> UILabel {
+        let width = self.view.bounds.width - 40
+        let height = self.view.bounds.height
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: height))
+        messageLabel.text = message
+        messageLabel.textColor = .gray
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        messageLabel.font = UIFont(name: "Arial", size: size)
+        messageLabel.sizeToFit()
+        messageLabel.center.x = self.view.center.x
+        messageLabel.center.y = self.view.center.y + offset
+        return messageLabel
+    }
+}
